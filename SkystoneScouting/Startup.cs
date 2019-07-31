5ceecaecfece0b82c8f3f6db7d09e0c804f49ad4
Mihnea Theodor Visoiu <visoiu.mihnea@gmail.com>
2019-07-31 10:02:34 +0300

    Added browser link for dev purposes

--- SkystoneScouting/Startup.cs
+++ SkystoneScouting/Startup.cs
@@ -44,6 +44,7 @@ namespace SkystoneScouting
             {
                 app.UseDeveloperExceptionPage();
                 app.UseDatabaseErrorPage();
+                app.UseBrowserLink();
             }
             else
             {
