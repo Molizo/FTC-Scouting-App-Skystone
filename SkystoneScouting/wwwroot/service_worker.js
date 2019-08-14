b03b59b560e44ff909de4abc3a1ddb1237451493
Mihnea Theodor Visoiu <visoiu.mihnea@gmail.com>
2019-08-14 17:40:21 +0300

    Updated Manifest with offline page

--- SkystoneScouting/wwwroot/service_worker.js
+++ SkystoneScouting/wwwroot/service_worker.js
@@ -1,6 +1,51 @@
-﻿self.addEventListener('fetch', event => {
+﻿// This is the "Offline page" service worker
+
+const CACHE = "sftcsoutingapp-page";
+const offlineFallbackPage = "offline.html";
+
+// Install stage sets up the offline page in the cache and opens a new cache
+self.addEventListener("install", function (event) {
+    console.log("[FTC Scouting App] Install Event processing");
+
+    event.waitUntil(
+        caches.open(CACHE).then(function (cache) {
+            console.log("[FTC Scouting App] Cached offline page during install");
+
+            return cache.add(offlineFallbackPage);
+        })
+    );
+});
+
+// If any fetch fails, it will show the offline page.
+self.addEventListener("fetch", function (event) {
+    if (event.request.method !== "GET") return;
+
     event.respondWith(
-        caches.match(event.request, { ignoreSearch: true }).then(response => {
-            return response || fetch(event.request);
-        }))
+        fetch(event.request).catch(function (error) {
+            // The following validates that the request was for a navigation to a new document
+            if (
+                event.request.destination !== "document" ||
+                event.request.mode !== "navigate"
+            ) {
+                return;
+            }
+
+            console.error("[FTC Scouting App] Network request Failed. Serving offline page " + error);
+            return caches.open(CACHE).then(function (cache) {
+                return cache.match(offlineFallbackPage);
+            });
+        })
+    );
+});
+
+// This is an event that can be fired from your page to tell the SW to update the offline page
+self.addEventListener("refreshOffline", function () {
+    const offlinePageRequest = new Request(offlineFallbackPage);
+
+    return fetch(offlineFallbackPage).then(function (response) {
+        return caches.open(CACHE).then(function (cache) {
+            console.log("[FTC Scouting App] Offline page updated from refreshOffline event: " + response.url);
+            return cache.put(offlinePageRequest, response);
+        });
+    });
 });
