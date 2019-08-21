bae2b5c907a47c26068faa4d73f147737bea83f9
Mihnea Theodor Visoiu <visoiu.mihnea@gmail.com>
2019-08-21 21:57:25 +0300

    Implemented SW asset caching for faster response times

--- SkystoneScouting/wwwroot/service_worker.js
+++ SkystoneScouting/wwwroot/service_worker.js
@@ -1,7 +1,32 @@
-﻿// This is the "Offline page" service worker
+﻿const CACHE = "ftcsoutingapp";
 
-const CACHE = "sftcsoutingapp-page";
 const offlineFallbackPage = "/error/offline.html";
+
+const offlineResources = [
+    '/error/offline.html',
+    '/css/style.css',
+    '/css/materialize.css',
+    '/css/themes/theme-quantum.css',
+    '/plugins/animate-css/animate.css',
+    '/vendor/node-waves/dist/waves.css',
+    'https://cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.1.0/cookieconsent.min.css',
+    'https://cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.1.0/cookieconsent.min.js',
+    'https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css',
+    'https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js',
+    'https://code.jquery.com/jquery-3.4.1.min.js',
+    'https://fonts.googleapis.com/css?family=Roboto:400,700&subset=latin,cyrillic-ext',
+    'https://fonts.googleapis.com/icon?family=Material+Icons',
+    '/vendor/jquery-slimscroll/jquery.slimscroll.js',
+    '/vendor/node-waves/dist/waves.js',
+    '/js/admin.js',
+    '/js/demo.js',
+    '/media/user-img-background.png',
+    '/favicon.ico',
+    '/manifest.json',
+    '/service_worker.js',
+    '/docs',
+    '/legal'
+]
 
 // Install stage sets up the offline page in the cache and opens a new cache
 self.addEventListener("install", function (event) {
@@ -9,19 +34,23 @@ self.addEventListener("install", function (event) {
 
     event.waitUntil(
         caches.open(CACHE).then(function (cache) {
-            console.log("[FTC Scouting App] Cached offline page during install");
+            console.log("[FTC Scouting App] Cached website resources");
 
-            return cache.add(offlineFallbackPage);
+            return cache.addAll(offlineResources);
         })
     );
 });
+
 
 // If any fetch fails, it will show the offline page.
 self.addEventListener("fetch", function (event) {
     if (event.request.method !== "GET") return;
 
     event.respondWith(
-        fetch(event.request).catch(function (error) {
+        // Try the cache
+        caches.match(event.request).then(function (response) {
+            return response || fetch(event.request);
+        }).catch(function (error) {
             // The following validates that the request was for a navigation to a new document
             if (
                 event.request.destination !== "document" ||
